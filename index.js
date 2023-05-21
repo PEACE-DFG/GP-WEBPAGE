95dad5f92d75ff5ecaddd0caea180a06f16343aa
PEACE-DFG <awofesobipeace@gmail.com>
2023-05-21 18:51:02 +0100

    updated js

---
+++ index.js
@@ -0,0 +1,54 @@
+
+function mouseover(){
+    document.getElementById('home').style.color="red"
+}
+function mouseout(){
+    document.getElementById('home').style.color="white"
+}
+
+function mouseovert(){
+    document.getElementById('about').style.color="red"
+}
+function mouseoutt(){
+    document.getElementById('about').style.color="white"
+}
+function mouseoverr(){
+    document.getElementById('service').style.color="red"
+}
+function mouseoutr(){
+    document.getElementById('service').style.color="white"
+}
+function mouseoverm(){
+    document.getElementById('pot').style.color="red"
+    document.getElementById('pot').style.animationPlayState='running'
+}
+function mouseoutm(){
+    document.getElementById('pot').style.color="white"
+}
+function mouseoverq(){
+    document.getElementById('team').style.color="red"
+}
+function mouseoutq(){
+    document.getElementById('team').style.color="white"
+}
+function mouseoverv(){
+    document.getElementById('cv').style.color="red"
+}
+function mouseoutv(){
+    document.getElementById('cv').style.color="white"
+}
+function mouseoverw(){
+    document.getElementById('contact').style.color="red"
+}
+function mouseoutw(){
+    document.getElementById('contact').style.color="white"
+}
+
+// function text(){
+//     document.getElementById('over').style.display="block"
+//     document.getElementById('over').style.position="absolute"
+// }
+// function disappear(){
+//     document.getElementById('over').style.display="none"
+
+// }
